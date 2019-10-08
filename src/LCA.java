7c2ccaeea7e2b0ae5a9ef75dd18efe1ede086a22
stephenk1226 <stphnkinsella@gmail.com>
2019-10-08 15:23:03 +0100

    Added the functions to return the internal and actual LCA of a Binary Tree

--- src/LCA.java
+++ src/LCA.java
@@ -15,6 +15,70 @@ public class LCA {
 		}
 	}
 	
+	public static class NoParent
+	{
+		Node root; 
+	    private List<Integer> path1 = new ArrayList<>(); 
+	    private List<Integer> path2 = new ArrayList<>(); 
+	  
+	    
+	    int findLCA(int n1, int n2)
+	    { 
+	        path1.clear(); 
+	        path2.clear(); 
+	        return findLCAInternal(root, n1, n2); 
+	    } 
+	  
+	    private int findLCAInternal(Node root, int n1, int n2)
+	    { 
+	  
+	        if (!findPath(root, n1, path1) || !findPath(root, n2, path2)) 
+	        { 
+	            System.out.println((path1.size() > 0) ? "Node 1 is present" : "Node 1 is missing"); 
+	            System.out.println((path2.size() > 0) ? "Node 2 is present" : "Node 2 is missing"); 
+	            return -1; 
+	        } 
+	  
+	        int i; 
+	        for (i = 0; i < path1.size() && i < path2.size(); i++) 
+	        {       
+	            if (!path1.get(i).equals(path2.get(i))) 
+	                break; 
+	        } 
+	  
+	        return path1.get(i-1); 
+	    } 
+	    
+	    private boolean findPath(Node root, int n, List<Integer> path) 
+	    {  
+	        if (root == null) 
+	        { 
+	            return false; 
+	        } 
+	         
+	        path.add(root.data); 
+	  
+	        if (root.data == n) 
+	        { 
+	            return true; 
+	        } 
+	  
+	        if (root.left != null && findPath(root.left, n, path)) 
+	        { 
+	            return true; 
+	        } 
+	  
+	        if (root.right != null && findPath(root.right, n, path)) 
+	        { 
+	            return true; 
+	        }
+	        
+	        path.remove(path.size()-1); 
+	  
+	        return false; 
+	    }
+	}    
+	    
 	public static void main(String[] args) {
 		
 
