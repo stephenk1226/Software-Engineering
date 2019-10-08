8718916acf674d48958e522dbea0d3a8794f4f69
stephenk1226 <stphnkinsella@gmail.com>
2019-10-08 14:55:32 +0100

    Setting up the Binary Tree Data Structure

---
+++ src/LCA.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class LCA {
+	
+	public static class Node {
+		int data;
+		Node left, right;
+		
+		Node(int value)
+		{
+			data = value;
+			left = right = null;
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
