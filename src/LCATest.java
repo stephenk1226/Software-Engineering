f09c79c49d6af4286f13f8a6ffd5c899e2d5f6dd
stephenk1226 <stphnkinsella@gmail.com>
2019-10-09 15:54:32 +0100

    Added a test based on the binary Tree example in lecture slides

--- src/LCATest.java
+++ src/LCATest.java
@@ -34,4 +34,50 @@ public class LCATest {
 		assertEquals(1, binaryTree.findLCA(7, 5));
 	}
 
+	@Test 
+	public void testTwo() {
+		
+		//Test using the example from the lecture slides 
+		
+		LCA.NoParent binaryTree = new LCA.NoParent();
+		binaryTree.root = new LCA.Node(1);
+		binaryTree.root.left = new LCA.Node(2);
+		binaryTree.root.left.left =  new LCA.Node(4);
+		binaryTree.root.left.left.left =  new LCA.Node(6);
+		binaryTree.root.right = new LCA.Node(3);
+		binaryTree.root.right.left = new LCA.Node(5);
+		binaryTree.root.right.left.right = new LCA.Node(8);
+		binaryTree.root.right.left.left = new LCA.Node(7);
+		binaryTree.root.right.left.left.right = new LCA.Node(10);
+		binaryTree.root.right.left.left.right.left = new LCA.Node(9);
+		binaryTree.root.right.left.left.right.right = new LCA.Node(11);
+		binaryTree.root.right.left.left.right.right.right = new LCA.Node(12);
+		
+		
+		/*
+		 * 						  1
+		 * 						/	\
+		 * 					   2	 3
+		 * 					  /     /
+		 * 					 4     5
+		 * 					/     / \
+		 * 				   6     7   8
+		 * 	                      \
+		 * 						   10
+		 * 						  /  \
+		 *                       9    11
+		 *                              \
+		 *                               12
+		 */					      
+		
+		
+		
+		assertEquals(5, binaryTree.findLCA(12, 8));
+		assertEquals(5, binaryTree.findLCA(9, 8));
+		assertEquals(10, binaryTree.findLCA(9, 12));
+		assertEquals(1, binaryTree.findLCA(6, 7));
+		assertEquals(5, binaryTree.findLCA(7, 8));
+				
+		
+	}
 }
